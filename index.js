c864dda8ae4eb26cfed04ad4a9fc4e2f3f9ef267
alexandre-notor <amanichambu48@gmail.com>
2022-10-21 16:25:12 +0200

    refactory: delete leanding blank

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
-
-
 app.use((req, _, next) => {
     console.log(`${req.method}  ${req.url}`);
     next();
@@ -16,7 +14,6 @@ app.use(routes);
 app.use((_, res) => {
     res.render('404');
 })
-
 
 app.listen(process.env.PORT || 3005, () => {
     console.log("Server starts on port " + (process.env.PORT || 3005));
